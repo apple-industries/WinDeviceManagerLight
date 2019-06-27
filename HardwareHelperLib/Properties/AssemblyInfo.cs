a0199507c12abed2a97d1c311dd5ae83d904351d
Rosario Pulella <rosariopulella@APPLE-LT2>
2019-06-27 10:57:50 -0400

    Configured the nuspec and assemblyInfo for packaging.

--- HardwareHelperLib/Properties/AssemblyInfo.cs
+++ HardwareHelperLib/Properties/AssemblyInfo.cs
@@ -5,9 +5,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("HardwareHelperLib")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Library for controlling windows devices.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Apple Industries, Inc.")]
 [assembly: AssemblyProduct("HardwareHelperLib")]
 [assembly: AssemblyCopyright("Copyright ©  2007 by William J Francis")]
 [assembly: AssemblyTrademark("")]
